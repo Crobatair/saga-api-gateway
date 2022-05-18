44035a12dd1bc23858b5bd63a5674a013ff448f1
Ale Vasquez <jose5m45@gmail.com>
2022-05-17 19:53:44 -0600

    initialized project related to API Gateway with Spring Cloud.

---
+++ src/test/java/com/crobatair/saga/estore/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crobatair.saga.estore.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
